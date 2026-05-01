cbd1721169f3c9d89ebfa20caad3f12473f56e1f
Rohan Nihalani <rohan.ved.nihalani@gmail.com>
2026-05-01 11:39:55 -0700

    added ucsb dining commons fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "carillo",
+    name: "spaghetti meatballs",
+    station: "pasta",
+  },
+  threeMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "dlg",
+      name: "banana cream pie",
+      station: "dessert",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "portola",
+      name: "eggs",
+      station: "breakfast",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "ortega",
+      name: "hamburger",
+      station: "main",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
