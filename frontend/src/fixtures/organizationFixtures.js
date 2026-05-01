a71c664fae2a8d188bc74c35e36bdc1993eb11d1
Pranav Gunhal <pgunhal@ucsb.edu>
2026-04-30 19:08:28 -0700

    add UCSB Organization Fixture

---
+++ frontend/src/fixtures/organizationFixtures.js
@@ -0,0 +1,33 @@
+const organizationFixtures = {
+  oneOrganization: [
+    {
+      id: 1,
+      name: "SB Hacks",
+      description: "Student run Hackathon at UCSB",
+    },
+  ],
+
+  threeRestaurants: [
+    {
+      id: 2,
+      name: "ACM",
+      description:
+        "The main CS club at UCSB.  They have a lot of fun events and are a great way to meet other CS students",
+    },
+
+    {
+      id: 3,
+      name: "Data Science Club",
+      description: "A club for students interested in data science.  They have workshops and projects related to data science and machine learning",
+    },
+
+    {
+      id: 4,
+      name: "UCSB Sports Analytics Club",
+      description:
+        "A club for students interested in sports analytics.  They have workshops and projects related to sports analytics and machine learning",
+    },
+  ],
+};
+
+export { organizationFixtures };
