cb3b08b8d2ba2ec6bf315a8639ffc018b8df6310
Joaquin Wong <joaquinwong@ucsb.edu>
2026-05-05 14:40:40 -0700

    jw added Edit Page to MenuItemReviews, works on Localhost and Storybook, passes npm test and npx run stryker

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsEditPage.jsx
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsEditPage.jsx
@@ -1,11 +1,79 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router";
+import MenuItemReviewForm from "main/components/MenuItemReviews/MenuItemReviewForm";
+import { Navigate } from "react-router";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewsEditPage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemReviewsEditPage({ storybook = false }) {
+  let { id } = useParams();
+
+  const {
+    data: menuItemReviews,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/menuitemreviews?id=${id}`],
+    {
+      // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+      method: "GET",
+      url: `/api/menuitemreviews`,
+      params: {
+        id,
+      },
+    },
+  );
+
+  const objectToAxiosPutParams = (menuItemReviews) => ({
+    url: "/api/menuitemreviews",
+    method: "PUT",
+    params: {
+      id: menuItemReviews.id,
+    },
+    data: {
+      itemId: menuItemReviews.itemId,
+      reviewerEmail: menuItemReviews.reviewerEmail,
+      stars: menuItemReviews.stars,
+      dateReviewed: menuItemReviews.dateReviewed,
+      comments: menuItemReviews.comments,
+    },
+  });
+
+  const onSuccess = (menuItemReviews) => {
+    toast(
+      `MenuItemReview Updated - id: ${menuItemReviews.id} comments: ${menuItemReviews.comments}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/menuitemreviews?id=${id}`],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreviews" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit MenuItemReview</h1>
+        {menuItemReviews && (
+          <MenuItemReviewForm
+            submitAction={onSubmit}
+            buttonLabel={"Update"}
+            initialContents={menuItemReviews}
+          />
+        )}
       </div>
     </BasicLayout>
   );
--- frontend/src/tests/pages/MenuItemReviews/MenuItemReviewsEditPage.test.jsx
+++ frontend/src/tests/pages/MenuItemReviews/MenuItemReviewsEditPage.test.jsx
@@ -1,47 +1,231 @@
-import { render, screen } from "@testing-library/react";
-import MenuItemReviewsEditPage from "main/pages/MenuItemReviews/MenuItemReviewsEditPage";
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
+import MenuItemReviewsEditPage from "main/pages/MenuItemReviews/MenuItemReviewsEditPage";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
+import mockConsole from "tests/testutils/mockConsole";
 
-describe("MenuItemReviewsEditPage tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
-
-  const setupUserOnly = () => {
-    axiosMock.reset();
-    axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
+const mockToast = vi.fn();
+vi.mock("react-toastify", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
   };
+});
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
+const mockNavigate = vi.fn();
+vi.mock("react-router", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    useParams: vi.fn(() => ({
+      id: 3,
+    })),
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
 
-    setupUserOnly();
+let axiosMock;
+describe("MenuItemReviewsEditPage tests", () => {
+  describe("when the backend doesn't return data", () => {
+    beforeEach(() => {
+      axiosMock = new AxiosMockAdapter(axios);
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock.onGet("/api/menuitemreviews", { params: { id: 3 } }).timeout();
+    });
 
-    // act
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <MenuItemReviewsEditPage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
+    afterEach(() => {
+      mockToast.mockClear();
+      mockNavigate.mockClear();
+      axiosMock.restore();
+      axiosMock.resetHistory();
+    });
 
-    // assert
-    await screen.findByText("Edit page not yet implemented");
-    expect(
-      screen.getByText("Edit page not yet implemented"),
-    ).toBeInTheDocument();
+    const queryClient = new QueryClient();
+    test("renders header but table is not present", async () => {
+      const restoreConsole = mockConsole();
+
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <MenuItemReviewsEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+      await screen.findByText("Edit MenuItemReview");
+      expect(
+        screen.queryByTestId("MenuItemReview-Comments"),
+      ).not.toBeInTheDocument();
+      restoreConsole();
+    });
+  });
+
+  describe("tests where backend is working normally", () => {
+    beforeEach(() => {
+      axiosMock = new AxiosMockAdapter(axios);
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock
+        .onGet("/api/menuitemreviews", { params: { id: 3 } })
+        .reply(200, {
+          id: 3,
+          itemId: 4,
+          reviewerEmail: "joaquinwong@ucsb.edu",
+          stars: 5,
+          dateReviewed: "2022-02-02T00:00",
+          comments: "Good",
+        });
+      axiosMock.onPut("/api/menuitemreviews").reply(200, {
+        id: 3,
+        itemId: 4,
+        reviewerEmail: "johndoe@ucsb.edu",
+        stars: 5,
+        dateReviewed: "2022-02-02T00:00",
+        comments: "Okay",
+      });
+    });
+
+    afterEach(() => {
+      mockToast.mockClear();
+      mockNavigate.mockClear();
+      axiosMock.restore();
+      axiosMock.resetHistory();
+    });
+
+    const queryClient = new QueryClient();
+
+    test("Is populated with the data provided", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <MenuItemReviewsEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+
+      await screen.findByTestId("MenuItemReviewForm-id");
+
+      const idField = screen.getByTestId("MenuItemReviewForm-id");
+      const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId");
+      const reviewerEmailField = screen.getByLabelText("ReviewerEmail");
+      const starsField = screen.getByLabelText("Stars");
+      const dateReviewedField = screen.getByLabelText(
+        "DateReviewed(iso format)",
+      );
+      const commentsField = screen.getByLabelText("Comments");
+      const submitButton = screen.getByText("Update");
+
+      expect(idField).toBeInTheDocument();
+      expect(idField).toHaveValue("3");
+      expect(itemIdField).toBeInTheDocument();
+      expect(itemIdField).toHaveValue("4");
+      expect(reviewerEmailField).toBeInTheDocument();
+      expect(reviewerEmailField).toHaveValue("joaquinwong@ucsb.edu");
+      expect(starsField).toBeInTheDocument();
+      expect(starsField).toHaveValue("5");
+      expect(dateReviewedField).toBeInTheDocument();
+      expect(dateReviewedField).toHaveValue("2022-02-02T00:00");
+      expect(commentsField).toBeInTheDocument();
+      expect(commentsField).toHaveValue("Good");
+
+      expect(submitButton).toHaveTextContent("Update");
+
+      fireEvent.change(reviewerEmailField, {
+        target: { value: "johndoe@ucsb.edu" },
+      });
+      fireEvent.change(commentsField, {
+        target: { value: "Okay" },
+      });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => expect(mockToast).toBeCalled());
+      expect(mockToast).toBeCalledWith(
+        "MenuItemReview Updated - id: 3 comments: Okay",
+      );
+
+      expect(mockNavigate).toBeCalledWith({ to: "/menuitemreviews" });
+
+      expect(axiosMock.history.put.length).toBe(1); // times called
+      expect(axiosMock.history.put[0].params).toEqual({ id: 3 });
+      expect(axiosMock.history.put[0].data).toBe(
+        JSON.stringify({
+          itemId: 4,
+          reviewerEmail: "johndoe@ucsb.edu",
+          stars: 5,
+          dateReviewed: "2022-02-02T00:00",
+          comments: "Okay",
+        }),
+      ); // posted object
+    });
+
+    test("Changes when you click Update", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <MenuItemReviewsEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+
+      await screen.findByTestId("MenuItemReviewForm-id");
+
+      const idField = screen.getByTestId("MenuItemReviewForm-id");
+      const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId");
+      const reviewerEmailField = screen.getByLabelText("ReviewerEmail");
+      const starsField = screen.getByLabelText("Stars");
+      const dateReviewedField = screen.getByLabelText(
+        "DateReviewed(iso format)",
+      );
+      const commentsField = screen.getByLabelText("Comments");
+      const submitButton = screen.getByText("Update");
+
+      expect(idField).toBeInTheDocument();
+      expect(idField).toHaveValue("3");
+      expect(itemIdField).toBeInTheDocument();
+      expect(itemIdField).toHaveValue("4");
+      expect(reviewerEmailField).toBeInTheDocument();
+      expect(reviewerEmailField).toHaveValue("joaquinwong@ucsb.edu");
+      expect(starsField).toBeInTheDocument();
+      expect(starsField).toHaveValue("5");
+      expect(dateReviewedField).toBeInTheDocument();
+      expect(dateReviewedField).toHaveValue("2022-02-02T00:00");
+      expect(commentsField).toBeInTheDocument();
+      expect(commentsField).toHaveValue("Good");
+
+      fireEvent.change(reviewerEmailField, {
+        target: { value: "johndoe@ucsb.edu" },
+      });
+      fireEvent.change(commentsField, {
+        target: { value: "Okay" },
+      });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => expect(mockToast).toBeCalled());
+      expect(mockToast).toBeCalledWith(
+        "MenuItemReview Updated - id: 3 comments: Okay",
+      );
+      expect(mockNavigate).toBeCalledWith({ to: "/menuitemreviews" });
+    });
   });
 });
