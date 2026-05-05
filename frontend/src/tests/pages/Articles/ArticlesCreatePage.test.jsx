4b26d5941b306a70c19325f55ab9ece7583963fb
Nick Kamenica <nicholaskamenica@ucsb.edu>
2026-05-05 15:06:06 -0700

    add create page to articles

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.jsx
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.jsx
@@ -1,13 +1,52 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
-import { expect } from "vitest";
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+const mockToast = vi.fn();
+vi.mock("react-toastify", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
+  };
+});
+
+const mockNavigate = vi.fn();
+vi.mock("react-router", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
 
 describe("ArticlesCreatePage tests", () => {
-  const queryClient = new QueryClient();
+  const axiosMock = new AxiosMockAdapter(axios);
 
-  test("renders correctly", async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
+
+  test("renders without crashing", async () => {
+    const queryClient = new QueryClient();
+
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -16,9 +55,82 @@ describe("ArticlesCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    await screen.findByText("Create page not yet implemented");
-    expect(
-      screen.getByText("Create page not yet implemented"),
-    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticlesForm-title")).toBeInTheDocument();
+    });
+  });
+
+  test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+    const queryClient = new QueryClient();
+
+    const article = {
+      id: 5,
+      title: "Using React Hooks",
+      url: "https://reactjs.org/docs/hooks-intro.html",
+      explanation:
+        "An introduction to React Hooks and how to use them effectively",
+      email: "author@example.com",
+      dateAdded: "2022-01-02T12:00:00",
+    };
+
+    axiosMock.onPost("/api/articles/post").reply(202, article);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <ArticlesCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByTestId("ArticlesForm-title")).toBeInTheDocument();
+    });
+
+    const titleField = screen.getByTestId("ArticlesForm-title");
+    const urlField = screen.getByTestId("ArticlesForm-url");
+    const explanationField = screen.getByTestId("ArticlesForm-explanation");
+    const emailField = screen.getByTestId("ArticlesForm-email");
+    const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+    const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+    fireEvent.change(titleField, {
+      target: { value: "Using React Hooks" },
+    });
+    fireEvent.change(urlField, {
+      target: { value: "https://reactjs.org/docs/hooks-intro.html" },
+    });
+    fireEvent.change(explanationField, {
+      target: {
+        value: "An introduction to React Hooks and how to use them effectively",
+      },
+    });
+    fireEvent.change(emailField, {
+      target: { value: "author@example.com" },
+    });
+    fireEvent.change(dateAddedField, {
+      target: { value: "2022-01-02T12:00:00" },
+    });
+
+    expect(submitButton).toBeInTheDocument();
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].url).toBe("/api/articles/post");
+    expect(axiosMock.history.post[0].params).toEqual({
+      title: "Using React Hooks",
+      url: "https://reactjs.org/docs/hooks-intro.html",
+      explanation:
+        "An introduction to React Hooks and how to use them effectively",
+      email: "author@example.com",
+      dateAdded: "2022-01-02T12:00:00",
+    });
+
+    expect(mockToast).toBeCalledWith(
+      "New article Created - id: 5 title: Using React Hooks",
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/articles" });
   });
 });
