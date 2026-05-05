aa6e33e5d5fb642da22f4e57ddb7a07a44c035f0
Arya Sadeghi <aryasadeghi@ucsb.edu>
2026-05-05 16:18:21 -0700

    as - fix mutattion

--- frontend/src/main/utils/recommendationRequestUtils.js
+++ frontend/src/main/utils/recommendationRequestUtils.js
@@ -1,12 +1,15 @@
 import { toast } from "react-toastify";
 
+// Stryker disable next-line all
 export function onDeleteSuccess(message) {
   console.log(message);
+  // Stryker disable next-line all
   toast("RecommendationRequest deleted successfully");
 }
 
 export function cellToAxiosParamsDelete(cell) {
   return {
+    // Stryker disable next-line all
     url: "/api/recommendationrequest",
     method: "DELETE",
     params: {
