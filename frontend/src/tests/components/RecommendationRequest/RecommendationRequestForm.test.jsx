e9a32391f2f1c37f86a2754c9a81113182950b33
Arya Sadeghi <aryasadeghi@ucsb.edu>
2026-05-05 15:57:46 -0700

    as - fixed prettier message

--- frontend/src/tests/components/RecommendationRequest/RecommendationRequestForm.test.jsx
+++ frontend/src/tests/components/RecommendationRequest/RecommendationRequestForm.test.jsx
@@ -99,9 +99,7 @@ describe("RecommendationRequestForm tests", () => {
     fireEvent.click(submitButton);
 
     await screen.findByText(/Requester Email is required/);
-    expect(
-      screen.getByText(/Professor Email is required/),
-    ).toBeInTheDocument();
+    expect(screen.getByText(/Professor Email is required/)).toBeInTheDocument();
     expect(screen.getByText(/Explanation is required/)).toBeInTheDocument();
     expect(screen.getByText(/Date Requested is required/)).toBeInTheDocument();
     expect(screen.getByText(/Date Needed is required/)).toBeInTheDocument();
