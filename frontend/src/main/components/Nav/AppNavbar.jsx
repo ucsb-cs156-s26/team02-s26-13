76d6d1e488275d1e5480048d9015625a0a704f0d
Arya Sadeghi <aryasadeghi@ucsb.edu>
2026-05-06 17:58:57 -0700

    as - add rec placeholders

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -65,6 +65,7 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/restaurants">
                     Restaurants
                   </Nav.Link>
+                  <Nav.Link as={NavLink} to="/recommendationrequest">RecommendationRequest</Nav.Link>
                   <Nav.Link as={Link} to="/helprequest">
                     HelpRequest
                   </Nav.Link>
